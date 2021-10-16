09f72277c981a3ae403b72b5b87c050a68b30f79
Na Nazhou <nazhou.na9@gmail.com>
2021-10-16 17:32:12 +0800

    Fix compiler unused variable warning due to assert

--- lib/asyncaddrinfo/asyncaddrinfo.c
+++ lib/asyncaddrinfo/asyncaddrinfo.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <fcntl.h>
 #include <netdb.h>
 #include <pthread.h>
@@ -10,6 +9,13 @@
 #include <unistd.h>
 
 #include "asyncaddrinfo.h"
+
+#ifdef NDEBUG
+#define ASSERT(x) do { (void)sizeof(x);} while (0)
+#else
+#include <assert.h>
+#define ASSERT(x) assert(x)
+#endif
 
 struct asyncaddrinfo_resolution {
 	int return_fd;
@@ -34,12 +40,12 @@ static void *asyncaddrinfo_main(void *arg) {
 		res->err = getaddrinfo(res->node, res->service, res->hints, &res->addrs);
 		int return_fd = res->return_fd;
 		res->return_fd = -1;
-		assert(send(return_fd, &res, sizeof(res), MSG_EOR) == sizeof(res));
+		ASSERT(send(return_fd, &res, sizeof(res), MSG_EOR) == sizeof(res));
 		// Main thread now owns res
-		assert(!close(return_fd));
+		ASSERT(!close(return_fd));
 	}
-	assert(!len);
-	assert(!close(fd));
+	ASSERT(!len);
+	ASSERT(!close(fd));
 	return NULL;
 }
 
@@ -56,30 +62,30 @@ static void asyncaddrinfo_del(struct asyncaddrinfo_resolution *res) {
 }
 
 void asyncaddrinfo_init(size_t threads) {
-	assert(!asyncaddrinfo_threads);
+	ASSERT(!asyncaddrinfo_threads);
 
 	int fds[2];
-	assert(!socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, fds));
+	ASSERT(!socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, fds));
 	asyncaddrinfo_write_fd = fds[1];
 
 	asyncaddrinfo_num_threads = threads;
 	asyncaddrinfo_threads = malloc(asyncaddrinfo_num_threads * sizeof(*asyncaddrinfo_threads));
-	assert(asyncaddrinfo_threads);
+	ASSERT(asyncaddrinfo_threads);
 
 	for (size_t i = 0; i < asyncaddrinfo_num_threads; i++) {
 		int subfd = fcntl(fds[0], F_DUPFD_CLOEXEC, 0);
-		assert(subfd >= 0);
-		assert(!pthread_create(&asyncaddrinfo_threads[i], NULL, asyncaddrinfo_main, (void *) (intptr_t) subfd));
+		ASSERT(subfd >= 0);
+		ASSERT(!pthread_create(&asyncaddrinfo_threads[i], NULL, asyncaddrinfo_main, (void *) (intptr_t) subfd));
 	}
-	assert(!close(fds[0]));
+	ASSERT(!close(fds[0]));
 }
 
 void asyncaddrinfo_cleanup() {
-	assert(asyncaddrinfo_threads);
-	assert(!close(asyncaddrinfo_write_fd));
+	ASSERT(asyncaddrinfo_threads);
+	ASSERT(!close(asyncaddrinfo_write_fd));
 	asyncaddrinfo_write_fd = -1;
 	for (size_t i = 0; i < asyncaddrinfo_num_threads; i++) {
-		assert(!pthread_join(asyncaddrinfo_threads[i], NULL));
+		ASSERT(!pthread_join(asyncaddrinfo_threads[i], NULL));
 	}
 	free(asyncaddrinfo_threads);
 	asyncaddrinfo_threads = NULL;
@@ -87,20 +93,20 @@ void asyncaddrinfo_cleanup() {
 
 int asyncaddrinfo_resolve(const char *node, const char *service, const struct addrinfo *hints) {
 	int fds[2];
-	assert(!socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, fds));
+	ASSERT(!socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, fds));
 
 	struct asyncaddrinfo_resolution *res = malloc(sizeof(*res));
-	assert(res);
+	ASSERT(res);
 	res->return_fd = fds[1];
 	if (node) {
 		res->node = strdup(node);
-		assert(res->node);
+		ASSERT(res->node);
 	} else {
 		res->node = NULL;
 	}
 	if (service) {
 		res->service = strdup(service);
-		assert(res->service);
+		ASSERT(res->service);
 	} else {
 		res->service = NULL;
 	}
@@ -110,7 +116,7 @@ int asyncaddrinfo_resolve(const char *node, const char *service, const struct ad
 	} else {
 		res->hints = NULL;
 	}
-	assert(send(asyncaddrinfo_write_fd, &res, sizeof(res), MSG_EOR) == sizeof(res));
+	ASSERT(send(asyncaddrinfo_write_fd, &res, sizeof(res), MSG_EOR) == sizeof(res));
 	// Resolve thread now owns res
 
 	return fds[0];
@@ -118,8 +124,9 @@ int asyncaddrinfo_resolve(const char *node, const char *service, const struct ad
 
 int asyncaddrinfo_result(int fd, struct addrinfo **addrs) {
 	struct asyncaddrinfo_resolution *res;
-	assert(recv(fd, &res, sizeof(res), 0) == sizeof(res));
-	assert(!close(fd));
+	ASSERT(recv(fd, &res, sizeof(res), 0) == sizeof(res));
+	ASSERT(!close(fd));
+
 	*addrs = res->addrs;
 	int err = res->err;
 	asyncaddrinfo_del(res);
